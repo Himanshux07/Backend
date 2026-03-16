7fcd6d96b23666f1f28253068a88d4fdbe2b9c0c
Himanshux07 <annikumar997@gmail.com>
2026-03-17 00:39:30 +0530

    feat: add route for adding video to watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { LogoutUser, registerUser, LoginUser,updateAvatar,updateCoverImage,updateUserProfile,changePassword,getCurrentUser, getUserChannelprofile, getWatchHistory } from "../controllers/user.controller.js"
+import { LogoutUser, registerUser, LoginUser,updateAvatar,updateCoverImage,updateUserProfile,changePassword,getCurrentUser, getUserChannelprofile, getWatchHistory, addVideoToWatchHistory } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
@@ -27,6 +27,7 @@ router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateAvatar)
 router.route("/cover-image").put(verifyJWT, upload.single("coverImage"), updateCoverImage)
 
 router.route("/c/:username").get(verifyJWT,getUserChannelprofile)
+router.route("/c/history/:videoId").post(verifyJWT, addVideoToWatchHistory)
 router.route("/c/history").get(verifyJWT, getWatchHistory)
 
 
