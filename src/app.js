a70a4de525fea9e88f743b19403cc770da0a508f
Himanshux07 <annikumar997@gmail.com>
2026-03-20 01:36:11 +0530

    feat: add controllers and routes for video, tweet, playlist, like, comment, and subscription functionalities

--- src/app.js
+++ src/app.js
@@ -16,7 +16,21 @@ app.use(cookieParser())
 
 //routes
 import userRouter from './routes/user.routes.js'
+import videoRouter from './routes/video.route.js'
+import playlistRouter from './routes/playlist.route.js'
+import subscriptionRouter from './routes/subscription.route.js'
+import tweetRouter from './routes/tweet.route.js'
+import likeRouter from './routes/like.route.js'
+import commentRouter from './routes/comment.route.js'
+
 //routes declaration
 app.use("/api/v1/user",userRouter)
+app.use("/api/v1/video",videoRouter)
+app.use("/api/v1/playlist",playlistRouter)
+app.use("/api/v1/subscription",subscriptionRouter)
+app.use("/api/v1/tweet",tweetRouter)
+app.use("/api/v1/like",likeRouter)
+app.use("/api/v1/comment",commentRouter)
+
 
 export { app }
