27cfb11fe3e4ce9d17d0a0f03a214981e3561c2f
Himanshux07 <annikumar997@gmail.com>
2026-03-22 16:41:59 +0530

    feat: add validation and response for fetching channel subscribers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -47,7 +47,47 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const { channelId } = req.params
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    const channel = await User.findById(channelId)
+    if (!channel) {
+        throw new ApiError(404, "Channel not found")
+    }
+
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscriber"
+            }
+        },
+        {
+            $unwind: "$subscriber"
+        },
+        {
+            $project: {
+                _id: "$subscriber._id",
+                username: "$subscriber.username",
+                avatar: "$subscriber.avatar",
+                fullname: "$subscriber.fullname"
+            }
+        }
+    ])
+
+    return res.status(200).json(
+        new ApiResponse(200, subscribers, "Subscriber list fetched successfully")
+    )
 })
 
 // controller to return channel list to which user has subscribed
